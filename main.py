bb9cffa7bfccad01a74347aa06dbf51d6b1847a1
qxvzol <ollie@speckledfrog.com>
2024-11-12 21:21:15 +0000

    Web server LED control

--- main.py
+++ main.py
@@ -1,14 +1,84 @@
+import network
+import socket
+import time
+
 from machine import Pin
-from utime import sleep
 
-pin = Pin("LED", Pin.OUT)
+from secrets import ssid, password
 
-print("LED starts flashing...")
+led = Pin("LED", Pin.OUT)
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+
+html = """
+        <!DOCTYPE html>
+        <html>
+        <form action="./lighton">
+        <input type="submit" value="Light on" />
+        </form>
+        <form action="./lightoff">
+        <input type="submit" value="Light off" />
+        </form>
+        <p>LED is %s</p>
+        </body>
+        </html>
+        """
+
+max_wait = 10
+while max_wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    max_wait -= 1
+    print('waiting for connection...')
+    time.sleep(1)
+
+if wlan.status() != 3:
+    raise RuntimeError('network connection failed')
+else:
+    print('connected')
+    status = wlan.ifconfig()
+    print( 'ip = ' + status[0] )
+
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+
+print('listening on', addr)
+stateis = "LED is OFF"
+# Listen for connections
 while True:
     try:
-        pin.toggle()
-        sleep(0.1) # sleep 1sec
-    except KeyboardInterrupt:
-        break
-pin.off()
-print("Finished.")
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        request = cl.recv(1024)
+        print(request)
+
+        request = str(request)
+        led_on = request.find('/lighton')
+        led_off = request.find('/lightoff')
+        print( 'led on = ' + str(led_on))
+        print( 'led off = ' + str(led_off))
+
+        if led_on == 6:
+            print("led on")
+            led.value(1)
+            stateis = "ON"
+
+        if led_off == 6:
+            print("led off")
+            led.value(0)
+            stateis = "OFF"
+
+        response = html % stateis
+
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(response)
+        cl.close()
+
+    except OSError as e:
+        cl.close()
+        print('OSError connection closed')
